c103c013246273c04341491861e1f9bd50b5f5fe
OkBand <o.bandura42@gmail.com>
2018-02-26 23:49:29 +0300

    code added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var reg = equation.match(/\d/g);
+  var a = reg[1];
+  var b = reg[3];
+  var c = reg[4];
+  var d = Math.pow(b, 2) - 4 * a * c;
+
+  var dr = Math.sqrt(d);
+  var x1 = (-b + dr) / 2 * a;
+  var x2 = (-b - dr) / 2 * a;
+
+  return solutions [x1, x2];
 }
