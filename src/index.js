38859a5ac1f3b356f9b51fa5779e92e29e48f57f
Bandura <o.bandura@itransition.com>
2018-02-27 10:24:10 +0300

    spaces removed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = function solveEquation(equation) {
+  equation = equation.replace(/ /g,'');
   var reg = equation.match(/-?\d+/g);
   var a = reg[0];
   var b = reg[2];
@@ -6,8 +7,8 @@ module.exports = function solveEquation(equation) {
   var d = Math.pow(b, 2) - 4 * a * c;
 
   var dr = Math.sqrt(d);
-  var x1 = (-b + dr) / 2 * a;
-  var x2 = (-b - dr) / 2 * a;
+  var x1 = (-b + dr) / (2 * a);
+  var x2 = (-b - dr) / (2 * a);
 
   const solutions = [x1, x2];
 
